9c8aea806ec6d7f76ea5996e57a622bc61539def
Gabriel Gadelha <41694144+GabrielGadelha@users.noreply.github.com>
2023-05-08 10:10:06 -0300

    Update UserRepository.java

--- src/main/java/br/edu/ufersa/pw20222todoList/domain/repository/UserRepository.java
+++ src/main/java/br/edu/ufersa/pw20222todoList/domain/repository/UserRepository.java
@@ -9,5 +9,6 @@ public interface UserRepository extends JpaRepository<User,Long>{
 
 	User findByUuid(UUID id);
 	User findByEmail(String email);
+	User findByEmailAndSenha(String email, String senha);
 
 }
